60e0b00e4ef288ea4a74db311bf5b63d7721202f
userforpython47 <userforpython47@wsl>
2024-10-28 01:21:34 -0500

    Add waf service

--- terraform/waf.tf
+++ terraform/waf.tf
@@ -1,182 +1,188 @@
-//WAF Configuration
-# Configuración de los proveedores
 
-# Creación de la Web ACL CloudFront
+# # Define WAF ACL for cloudfront
+# resource "aws_wafv2_web_acl" "waf_cloudfront_bird_bogota" {
+#   name = lower("waf-cloudfront-${var.aws_resource_tags["project"]}-${var.aws_resource_tags["environment"]}-${random_string.id.result}")
 
-resource "aws_wafv2_web_acl" "waf_cloudfront_bird_bogota" {
-  name = lower("waf-cloudfront-${var.aws_resource_tags["project"]}-${var.aws_resource_tags["environment"]}-${random_string.id.result}")
+#   scope         = "CLOUDFRONT" #Define cloudfront scope
+#   default_action {
+#     allow {}
+#   }
 
-  scope         = "CLOUDFRONT"
-  default_action {
-    allow {}
-  }
+#     visibility_config {
+#     cloudwatch_metrics_enabled = true
+#     metric_name                = "waf_cloudfront_bird_bogota"
+#     sampled_requests_enabled   = true
+#   }
 
-    visibility_config {
-    cloudwatch_metrics_enabled = true
-    metric_name                = "waf_cloudfront_bird_bogota"
-    sampled_requests_enabled   = true
-  }
+# rule {
+#     name     = "AWS-AWSManagedRulesCommonRuleSet"
+#     priority = 0
 
-rule {
-    name     = "AWS-AWSManagedRulesCommonRuleSet"
-    priority = 0
+#     override_action {
+#       none {}
+#     }
 
-    override_action {
-      none {}
-    }
+#     statement {
+#       managed_rule_group_statement {
+#         name        = "AWSManagedRulesCommonRuleSet"
+#         vendor_name = "AWS"
 
-    statement {
-      managed_rule_group_statement {
-        name        = "AWSManagedRulesCommonRuleSet"
-        vendor_name = "AWS"
+#         rule_action_override {
+#           action_to_use {
+#             allow {}
+#           }
+#           name = "SizeRestrictions_BODY"
+#         }
+#       }
+#     }
+#     visibility_config {
+#       cloudwatch_metrics_enabled = true
+#       metric_name                = "AWS-AWSManagedRulesCommonRuleSet"
+#       sampled_requests_enabled   = true
+#     }
+#   }
 
-        rule_action_override {
-          action_to_use {
-            allow {}
-          }
-          name = "SizeRestrictions_BODY"
-        }
-      }
-    }
-    visibility_config {
-      cloudwatch_metrics_enabled = true
-      metric_name                = "AWS-AWSManagedRulesCommonRuleSet"
-      sampled_requests_enabled   = true
-    }
-  }
+#   rule {
+#     name     = "AWS-AWSManagedRulesAmazonIpReputationList"
+#     priority = 1
 
-#####
-  rule {
-    name     = "AWS-AWSManagedRulesAmazonIpReputationList"
-    priority = 1
+#     override_action {
+#       none {}
+#     }
 
-    override_action {
-      none {}
-    }
+#     statement {
+#       managed_rule_group_statement {
+#         name        = "AWSManagedRulesAmazonIpReputationList"
+#         vendor_name = "AWS"
+#       }
+#     }
+#     visibility_config {
+#       cloudwatch_metrics_enabled = true
+#       metric_name                = "AWS-AWSManagedRulesAmazonIpReputationList"
+#       sampled_requests_enabled   = true
+#     }
+#   }
 
-    statement {
-      managed_rule_group_statement {
-        name        = "AWSManagedRulesAmazonIpReputationList"
-        vendor_name = "AWS"
-      }
-    }
-    visibility_config {
-      cloudwatch_metrics_enabled = true
-      metric_name                = "AWS-AWSManagedRulesAmazonIpReputationList"
-      sampled_requests_enabled   = true
-    }
-  }
+#   rule {
+#     name     = "AWS-AWSManagedRulesSQLiRuleSet"
+#     priority = 2
 
-  rule {
-    name     = "AWS-AWSManagedRulesSQLiRuleSet"
-    priority = 2
+#     override_action {
+#       none {}
+#     }
 
-    override_action {
-      none {}
-    }
+#     statement {
+#       managed_rule_group_statement {
+#         name        = "AWSManagedRulesSQLiRuleSet"
+#         vendor_name = "AWS"
+#       }
+#     }
+#    visibility_config {
+#       cloudwatch_metrics_enabled = true
+#       metric_name                = "AWS-AWSManagedRulesSQLiRuleSet"
+#       sampled_requests_enabled   = true
+#     }
+#   }
 
-    statement {
-      managed_rule_group_statement {
-        name        = "AWSManagedRulesSQLiRuleSet"
-        vendor_name = "AWS"
-      }
-    }
-   visibility_config {
-      cloudwatch_metrics_enabled = true
-      metric_name                = "AWS-AWSManagedRulesSQLiRuleSet"
-      sampled_requests_enabled   = true
-    }
-  }
+# }
 
-}
+# # Create WAF ACL for APIGateway
+# resource "aws_wafv2_web_acl" "api_gateway_waf" {
+#   name = lower("waf-apigw-${var.aws_resource_tags["project"]}-${var.aws_resource_tags["environment"]}-${random_string.id.result}")
+
+#   description = "Managed rule WAF for ApiGateway"
+#   scope       = "REGIONAL" # Define regional scope
 
 
-# Create WAF for APIGateway
-resource "aws_wafv2_web_acl" "api_gateway_waf" {
-  name = lower("waf-apigw-${var.aws_resource_tags["project"]}-${var.aws_resource_tags["environment"]}-${random_string.id.result}")
+#   default_action {
+#     allow {}
+#   }
 
-  description = "Managed rule WAF for ApiGateway"
-  scope       = "REGIONAL"
+# rule {
+#     name     = "AWS-AWSManagedRulesCommonRuleSet"
+#     priority = 0
 
-  default_action {
-    allow {}
-  }
+#     override_action {
+#       none {}
+#     }
 
-rule {
-    name     = "AWS-AWSManagedRulesCommonRuleSet"
-    priority = 0
+#     statement {
+#       managed_rule_group_statement {
+#         name        = "AWSManagedRulesCommonRuleSet"
+#         vendor_name = "AWS"
 
-    override_action {
-      none {}
-    }
+#         rule_action_override {
+#           action_to_use {
+#             allow {}
+#           }
+#           name = "SizeRestrictions_BODY"
+#         }
+#       }
+#     }
+#     visibility_config {
+#       cloudwatch_metrics_enabled = true
+#       metric_name                = "AWS-AWSManagedRulesCommonRuleSet"
+#       sampled_requests_enabled   = true
+#     }
+#   }
 
-    statement {
-      managed_rule_group_statement {
-        name        = "AWSManagedRulesCommonRuleSet"
-        vendor_name = "AWS"
 
-        rule_action_override {
-          action_to_use {
-            allow {}
-          }
-          name = "SizeRestrictions_BODY"
-        }
-      }
-    }
-    visibility_config {
-      cloudwatch_metrics_enabled = true
-      metric_name                = "AWS-AWSManagedRulesCommonRuleSet"
-      sampled_requests_enabled   = true
-    }
-  }
+#   rule {
+#     name     = "AWS-AWSManagedRulesAmazonIpReputationList"
+#     priority = 1
 
-#####
-  rule {
-    name     = "AWS-AWSManagedRulesAmazonIpReputationList"
-    priority = 1
+#     override_action {
+#       none {}
+#     }
 
-    override_action {
-      none {}
-    }
+#     statement {
+#       managed_rule_group_statement {
+#         name        = "AWSManagedRulesAmazonIpReputationList"
+#         vendor_name = "AWS"
+#       }
+#     }
+#     visibility_config {
+#       cloudwatch_metrics_enabled = true
+#       metric_name                = "AWS-AWSManagedRulesAmazonIpReputationList"
+#       sampled_requests_enabled   = true
+#     }
+#   }
 
-    statement {
-      managed_rule_group_statement {
-        name        = "AWSManagedRulesAmazonIpReputationList"
-        vendor_name = "AWS"
-      }
-    }
-    visibility_config {
-      cloudwatch_metrics_enabled = true
-      metric_name                = "AWS-AWSManagedRulesAmazonIpReputationList"
-      sampled_requests_enabled   = true
-    }
-  }
+#   rule {
+#     name     = "AWS-AWSManagedRulesSQLiRuleSet"
+#     priority = 2
 
-  rule {
-    name     = "AWS-AWSManagedRulesSQLiRuleSet"
-    priority = 2
+#     override_action {
+#       none {}
+#     }
 
-    override_action {
-      none {}
-    }
+#     statement {
+#       managed_rule_group_statement {
+#         name        = "AWSManagedRulesSQLiRuleSet"
+#         vendor_name = "AWS"
+#       }
+#     }
+#    visibility_config {
+#       cloudwatch_metrics_enabled = true
+#       metric_name                = "AWS-AWSManagedRulesSQLiRuleSet"
+#       sampled_requests_enabled   = true
+#     }
+#   }
 
-    statement {
-      managed_rule_group_statement {
-        name        = "AWSManagedRulesSQLiRuleSet"
-        vendor_name = "AWS"
-      }
-    }
-   visibility_config {
-      cloudwatch_metrics_enabled = true
-      metric_name                = "AWS-AWSManagedRulesSQLiRuleSet"
-      sampled_requests_enabled   = true
-    }
-  }
+#     visibility_config {
+#     cloudwatch_metrics_enabled = true
+#     metric_name                = "apiGatewayWafMetrics"
+#     sampled_requests_enabled   = true
+#   }
 
-    visibility_config {
-    cloudwatch_metrics_enabled = true
-    metric_name                = "apiGatewayWafMetrics"
-    sampled_requests_enabled   = true
-  }
+# }
 
-}
+# # Associate WAF Web ACL with API Gateway
+# resource "aws_apigatewayv2_stage" "api_stage" {
+#   api_id      = aws_apigatewayv2_api.api_gateway.id
+#   name        = "dev"
+#   auto_deploy = true
+
+#   # WAF association
+#   web_acl_arn = aws_wafv2_web_acl.api_gateway_waf.arn
+# }
